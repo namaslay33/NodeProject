55c0ca4d552606a2a650474c5f61996cd0154e1e
Namaslay33 <skylar.hunt@utexas.edu>
2018-08-16 11:23:28 -0500

    Fixed my dog route. Info shows on the path now

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 let data = require('./data/dog.json');
 
-app.set('appData', data);
+app.set('dog', data);
 
 app.use('/public', express.static('public'));
 
 app.use(require('./routes/index'));
 app.use(require('./routes/dogs'));
+// app.use(require('./routes/feedback'));
+// app.use(require('./routes/api'));
 
 app.listen(7000, () =>{
     console.log('listening on port 7000');
--- routes/dogs.js
+++ routes/dogs.js
@@ -3,41 +3,44 @@ let router = express.Router();
 
 router.get('/dogs', function(request, response){
     
-    let dataFile = req.app.get("appData");
+    let dataFile = request.app.get("dog");
     let myHTML;
+    
 
-    dataFile.dogs.forEach(function(breed){
+    dataFile.forEach(function(item){
         myHTML += `
 
         <li>
-            <h2>${breed.Type}</h2>
-            <img src="../images/dogs/${breed.}
-            <p>${breed.Info}</p>
+            <h2>${item.Type}</h2>
+            <img src="../public/images/${item.Type}.jpg" alt="Dog Pics" style="width: 300px">
+            <p>${item.Info}</p>
         </li>
         `;
     })
 
+    
+
     response.send(`
     <h1>Life is Better with a Dog</h1>
-    <h4> Some of the Favorite Breeds </h4>
+    <h4> Some of the Popular Breeds </h4>
     
     <ul>
         ${myHTML}
     </ul>
-   
-    
     `)
 })
 
+    router.get('/dogs/:dogType', function(request, response){
 
-    router.get('/dogs/:type', function(request, response){
-
-        let dogFile = req.app.get("apData");
+        let dataFile = request.app.get("dog");
         let myHTML;
 
-        let dogs = dogFile.dog[req.params.type];
+        let dog = dataFile.dogs[request.params.dogType];
 
         response.send(`
+        <link rel="stylesheet" href="../public/css/dogs.css">
+        <h2>${dog.Type}</h2>
+        <p>${dog.Info}</p>
         Hello World
     `)
 })
